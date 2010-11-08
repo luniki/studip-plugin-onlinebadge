3405535791fd3e97cf06dcc3e5c3510a3c8b131c
mlunzena <mlunzena@8b9782fd-b62b-0410-a6c6-fea12034b7c6>
2010-11-08 10:31:37 +0000

    update js and css

--- javascripts/application.js
+++ javascripts/application.js
@@ -1,29 +1,11 @@
+jQuery(function($) {
+    var a = $('#barTopMenu li a[href="online.php"]')
+      , url = STUDIP.ABSOLUTE_URI_STUDIP + a.attr('href');
 
-jQuery(document).ready(function(){
-    var url;
-    jQuery('#barTopMenu').children().each(function(index) {
-        if (jQuery(this).children().attr('href') == 'online.php') {
-        
-            url = jQuery(this).children().attr('href');
-            jQuery(this).children().wrap('<div class="online_container" />');
-        }
-    }
-    );
-    jQuery('.online_container').append('<div id="online_badge" />');
-    jQuery('.online_container').bind('click', function(){
-        window.location = STUDIP.ABSOLUTE_URI_STUDIP + url;
-    });
-    jQuery('.online_container').bind('mouseover', function(){
-        jQuery('.online_container').children().css('color', 'white');
-        jQuery('.online_container').children().children().css('background-position', '-32px 0');
-    });
-    jQuery('.online_container').bind('mouseout', function(){
-        jQuery('.online_container').children().css('color', '#ABB7CE');
-        jQuery('.online_container').children().children().css('background-position', '0 0');
-         jQuery('#online_badge').css('color','white');
-    });
-    jQuery('#online_badge').css('cursor', 'pointer');
-    
-    jQuery('#online_badge').load(STUDIP.ABSOLUTE_URI_STUDIP + '/plugins.php/onlinebadge/main/buddies_online');
-    
+    a.parent()
+     .addClass("online_container")
+     .bind('click', function() { location = url; })
+     .append('<span id="online_badge"/>')
+     .children(":last")
+     .load(STUDIP.ABSOLUTE_URI_STUDIP + '/plugins.php/onlinebadge/main/buddies_online');
 });
